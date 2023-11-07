4ce5e365ad1112ce0d8f644c79f21ea939fb240b
hamzanasir08 <150170079+hamzanasir08@users.noreply.github.com>
2023-11-07 13:15:19 -0800

    Update QUESTION3.c

--- LAB2/QUESTION3.c
+++ LAB2/QUESTION3.c
@@ -1,8 +1,8 @@
 //Q3. Write a C program to compare two strings without using string library functions.
 int main()
 {
-    char str1[]="Talha";
-    char str2[]="Samad";
+    char str1[]="hamza";
+    char str2[]="nasir";
     int a=0;
     int sum=0;
     while(a<5){
